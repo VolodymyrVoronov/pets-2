7d348f961a8461cbd8f90dcad20e85410981f33c
Volodymyr Voronov <malones.vladimir@gmail.com>
2023-01-06 17:41:20 +0100

    chore(web/pages): updated AddPetPage.

--- packages/web/src/pages/AddPetPage/AddPetPage.tsx
+++ packages/web/src/pages/AddPetPage/AddPetPage.tsx
@@ -1,13 +1,23 @@
-import React from "react";
+import { FlexboxGrid, Col } from "rsuite";
 
 import trpc from "../../hooks/trpc";
 
 import AnimatedWrapper from "../../components/AnimatedWrapper/AnimatedWrapper";
 
+import styles from "./AddPetPage.module.css";
+
 const AddPetPage = (): JSX.Element => {
   const { mutate, isLoading, isError, error } = trpc.useMutation(["createPet"]);
 
-  return <AnimatedWrapper>AddPetPage</AnimatedWrapper>;
+  return (
+    <AnimatedWrapper>
+      <FlexboxGrid justify="center" className={styles["add-page"]}>
+        <FlexboxGrid.Item as={Col} colspan={24} xs={22} sm={18} md={16} lg={14}>
+          Lorem ipsum dolor sit, amet consectetur adipisicing elit. Quas in
+        </FlexboxGrid.Item>
+      </FlexboxGrid>
+    </AnimatedWrapper>
+  );
 };
 
 export default AddPetPage;
