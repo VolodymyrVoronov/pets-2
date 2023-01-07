a0a757f8bedac55371e9df81a5ab37f90b1b22e0
Volodymyr Voronov <malones.vladimir@gmail.com>
2023-01-07 14:34:48 +0100

    chore(web/api): updated Form, updated model.

---
+++ packages/api/prisma/migrations/20230107131034_init/migration.sql
@@ -0,0 +1,20 @@
+-- RedefineTables
+PRAGMA foreign_keys=OFF;
+CREATE TABLE "new_Pet" (
+    "id" INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
+    "name" TEXT NOT NULL,
+    "age" TEXT NOT NULL,
+    "breed" TEXT NOT NULL,
+    "photo" TEXT NOT NULL,
+    "diet" TEXT NOT NULL,
+    "diseases" TEXT NOT NULL,
+    "information" TEXT NOT NULL,
+    "isMarked" BOOLEAN NOT NULL DEFAULT false,
+    "createdAt" DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" DATETIME NOT NULL
+);
+INSERT INTO "new_Pet" ("age", "breed", "createdAt", "diet", "diseases", "id", "information", "isMarked", "name", "photo", "updatedAt") SELECT "age", "breed", "createdAt", "diet", "diseases", "id", "information", "isMarked", "name", "photo", "updatedAt" FROM "Pet";
+DROP TABLE "Pet";
+ALTER TABLE "new_Pet" RENAME TO "Pet";
+PRAGMA foreign_key_check;
+PRAGMA foreign_keys=ON;
